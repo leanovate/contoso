75e5efed0942a36bd647b455702bc785dca4e96f
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2017-02-17 15:16:03 +0100

    Initial commit after generating jhipster project

---
+++ src/test/java/de/leanovate/contoso/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,16 @@
+package de.leanovate.contoso.cucumber.stepdefs;
+
+import de.leanovate.contoso.ConferencemanagerApp;
+
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = ConferencemanagerApp.class, loader = SpringApplicationContextLoader.class)
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
